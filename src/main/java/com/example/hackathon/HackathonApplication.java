15eb3ac4ef6780f117852b286023edc1b35a0418
Vijayavel-Mohan <44271789+Vijayavel-Mohan@users.noreply.github.com>
2021-04-23 23:16:23 +0530

    Create HackathonApplication.java

---
+++ src/main/java/com/example/hackathon/HackathonApplication.java
@@ -0,0 +1,17 @@
+package com.example.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication(scanBasePackages="com.example")
+public class HackathonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HackathonApplication.class, args);
+	}
+
+}
